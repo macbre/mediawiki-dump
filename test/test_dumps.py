dd773acf2d96921c1be211d73b7b1143b17d311a
macbre <maciej.brencz@gmail.com>
2018-10-29 21:58:49 +0100

    DumpReader: handle full history dumps

--- test/test_dumps.py
+++ test/test_dumps.py
@@ -2,11 +2,22 @@ from mediawiki_dump.dumps import WikipediaDump, WikiaDump
 
 
 def test_dump_get_url():
+    # https://dumps.wikimedia.org/fowiki/latest/
     assert WikipediaDump('fo').get_url() == \
         'https://dumps.wikimedia.org/fowiki/latest/fowiki-latest-pages-meta-current.xml.bz2'
 
+    assert WikipediaDump('fo', full_history=False).get_url() == \
+        'https://dumps.wikimedia.org/fowiki/latest/fowiki-latest-pages-meta-current.xml.bz2'
+
+    assert WikipediaDump('fo', full_history=True).get_url() == \
+        'https://dumps.wikimedia.org/fowiki/latest/fowiki-latest-pages-meta-history.xml.bz2'
+
+    # https://poznan.wikia.com/wiki/Specjalna:Statystyka
     assert WikiaDump('plpoznan').get_url() == \
         'https://s3.amazonaws.com/wikia_xml_dumps/p/pl/plpoznan_pages_current.xml.7z'
+
+    assert WikiaDump('plpoznan', full_history=True).get_url() == \
+        'https://s3.amazonaws.com/wikia_xml_dumps/p/pl/plpoznan_pages_full.xml.7z'
 
 
 def test_get_cache_filename():
@@ -15,3 +26,6 @@ def test_get_cache_filename():
 
     dump = WikipediaDump('en')
     assert dump.get_cache_filename(dump.get_url()) == 'mediawiki_dump_7160d38a2668dcc15a33ee6e2a685bbd.bz2'
+
+    dump = WikipediaDump('en', full_history=True)
+    assert dump.get_cache_filename(dump.get_url()) == 'mediawiki_dump_84ec5ef573e2d2666b97f82874ae5d67.bz2'
