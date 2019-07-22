3ae6238502a445a659554a3728dd0e33ab008f3f
macbre <maciej.brencz@gmail.com>
2019-07-22 22:07:00 +0200

    v0.6.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.6.5'
+VERSION = '0.6.6'
 
 # @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
 with open("README.md", "r") as fh:
