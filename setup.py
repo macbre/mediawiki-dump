57fe9127bf69666b4f528dc1a5ad32907f6469c8
Maciej Brencz <maciej.brencz@gmail.com>
2021-06-15 11:31:16 +0100

    Bump to v0.8.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.7.0"
+VERSION = "0.8.0"
 
 # @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
 with open("README.md", "r") as fh:
