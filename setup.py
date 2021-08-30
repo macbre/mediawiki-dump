95fb5dfd7afa1214545a077155e73226fab10ab7
macbre <maciej.brencz@gmail.com>
2021-08-30 10:38:12 +0100

    Use pytest-cov and report code coverage (threshold set to %91)

--- setup.py
+++ setup.py
@@ -42,9 +42,10 @@ setup(
     extras_require={
         "dev": [
             "black==21.8b0",
-            "coverage==5.5",
+            "coveralls==3.2.0",
             "pylint==2.10.2",
             "pytest==6.2.4",
+            "pytest-cov==2.12.1",
         ]
     },
     install_requires=[
