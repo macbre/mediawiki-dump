c32cd8f08406573adaf11255f16d8b5c4db38c17
Maciej Brencz <maciej.brencz@gmail.com>
2022-06-17 10:12:15 +0100

    State clearly that we require Python 3.7+

--- setup.py
+++ setup.py
@@ -30,13 +30,8 @@ setup(
         "Topic :: Text Processing :: Markup :: XML",
         # Pick your license as you wish
         "License :: OSI Approved :: MIT License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
+    python_requires=">=3.7",
     packages=find_packages(),
     extras_require={
         "dev": [
